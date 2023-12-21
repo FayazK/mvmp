ff5654574f14499be8403e38148ee329e2ea7836
Fayaz Khan <info@fayazk.com>
2023-12-21 18:30:43 +0500

    🔧 chore(create_stores_table.php): add 'name' and 'user_id' columns to the 'stores' table for improved functionality 🔧 chore(create_stores_table.php): add foreign key constraint on 'user_id' column to ensure referential integrity with 'users' table

--- database/migrations/2023_12_21_132628_create_stores_table.php
+++ database/migrations/2023_12_21_132628_create_stores_table.php
@@ -12,6 +12,8 @@ return new class extends Migration {
     {
         Schema::create('stores', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
             $table->softDeletes();
         });
