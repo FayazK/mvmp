450d11931b4b75cdc042cb187f84e516e7c098b7
Fayaz Khan <info@fayazk.com>
2023-12-21 18:27:05 +0500

    📦 chore(models): add Currency, Role, and Store models
    📄 chore(migrations): create migrations for users, roles, currencies, and stores tables
    
    - Add Currency model to handle currency-related data
    - Add Role model to handle user roles and permissions
    - Add Store model to handle store-related data
    
    - Create migration for users table with additional columns: name (nullable), first_name, last_name (nullable)
    - Create migration for roles table with columns: name, slug, permissions (nullable)
    - Create migration for currencies table with columns: code (unique), name, exchange_rate, format, decimal_point, thousand_point, decimal_places (default: 2), enabled (default: 0), default (default: 0)
    - Create migration for stores table

---
+++ database/migrations/2023_12_21_132031_create_currencies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->decimal('exchange_rate', 10, 4);
+            $table->string('format');
+            $table->string('decimal_point');
+            $table->string('thousand_point');
+            $table->integer('decimal_places')->default(2)->index();
+            $table->boolean('enabled')->default(0)->index();
+            $table->boolean('default')->default(0)->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('currencies');
+    }
+};
